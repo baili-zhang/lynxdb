e820df494c002fbec2337c50fee13017701673bc
zhangbaili <1456938262@qq.com>
2021-12-17 00:16:24 +0800

    新增 Mdtp.encode() 的测试

---
+++ server/src/test/java/zbl/moonlight/server/protocol/MdtpTest.java
@@ -0,0 +1,49 @@
+package zbl.moonlight.server.protocol;
+
+import org.junit.jupiter.api.Test;
+import zbl.moonlight.server.command.Method;
+import zbl.moonlight.server.engine.buffer.DynamicByteBuffer;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MdtpTest {
+
+    @Test
+    void encode() throws EncodeException {
+        String key = "ByteBuffer byteBuffer";
+        String value = "ByteBuffer.wrap(\"abc\".getBytes(StandardCharsets.UTF_8))";
+        ByteBuffer byteBuffer = Mdtp.encode(Method.GET, ByteBuffer.wrap(key.getBytes(StandardCharsets.UTF_8)),
+                ByteBuffer.wrap(value.getBytes(StandardCharsets.UTF_8)));
+
+        assert byteBuffer.get(0) == Method.GET;
+
+        int keyLength = byteBuffer.get(1) & 0xff;
+        int valueLength = ((byteBuffer.get(2) & 0xff) << 24) |
+                ((byteBuffer.get(3) & 0xff) << 16) |
+                ((byteBuffer.get(4) & 0xff) << 8) |
+                (byteBuffer.get(5) & 0xff);
+
+        assert keyLength == key.length();
+        assert valueLength == value.length();
+
+        byte[] keyBytes = new byte[keyLength];
+        for (int i = Mdtp.HEADER_LENGTH; i < Mdtp.HEADER_LENGTH + keyLength; i ++) {
+            keyBytes[i-Mdtp.HEADER_LENGTH] = byteBuffer.get(i);
+        }
+        assert key.equals(new String(keyBytes));
+
+        byte[] valueBytes = new byte[valueLength];
+        int valueOffset = Mdtp.HEADER_LENGTH + keyLength;
+        for (int i = valueOffset; i < valueOffset + valueLength; i ++) {
+            valueBytes[i-valueOffset] = byteBuffer.get(i);
+        }
+        assert value.equals(new String(valueBytes));
+    }
+
+    @Test
+    void decode() {
+    }
+}
