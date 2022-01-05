2ed7eff801db2061981e29c2e77d49597a63a1a0
zhangbaili <1456938262@qq.com>
2022-01-05 22:00:05 +0800

    重新设计Executor和Executable接口

--- server/src/main/java/zbl/moonlight/server/executor/Executable.java
+++ server/src/main/java/zbl/moonlight/server/executor/Executable.java
@@ -1,7 +1,6 @@
 package zbl.moonlight.server.executor;
 
 /* 可执行接口，用来定义执行器 */
-public interface Executable<I, O> extends Runnable {
-    void offer(I in);
-    O poll();
+public interface Executable<E> extends Runnable {
+    void offer(E event);
 }
--- server/src/main/java/zbl/moonlight/server/executor/Executor.java
+++ server/src/main/java/zbl/moonlight/server/executor/Executor.java
@@ -3,42 +3,29 @@ package zbl.moonlight.server.executor;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 
-public abstract class Executor<I, O> implements Executable<I, O> {
+public abstract class Executor<E> implements Executable<E> {
     /* 输入队列 */
-    private final ConcurrentLinkedQueue<I> inQueue = new ConcurrentLinkedQueue<>();
-    /* 输出队列 */
-    private final ConcurrentLinkedQueue<O> outQueue = new ConcurrentLinkedQueue<>();
-    /* 执行完需要被通知的线程 */
-    private final Thread notifiedThread;
+    private final ConcurrentLinkedQueue<E> inQueue = new ConcurrentLinkedQueue<>();
+    /* 需要被通知的下游线程 */
+    private final Thread downStreamThread;
+    /* 下游的执行器 */
+    private final Executable<E> downStreamExecutor;
 
-    protected Executor(Thread notifiedThread) {
-        this.notifiedThread = notifiedThread;
+    protected Executor(Executable<E> downStreamExecutor, Thread downStreamThread) {
+        this.downStreamExecutor = downStreamExecutor;
+        this.downStreamThread = downStreamThread;
     }
 
     @Override
     /* 向输入队列中添加元素 */
-    public final void offer(I in) {
-        if(in != null) {
-            inQueue.offer(in);
-        }
-    }
-
-    @Override
-    /* 从输出队列队首移除元素 */
-    public final O poll() {
-        return outQueue.poll();
-    }
-
-    /* 向输出队列中添加元素 */
-    protected final void offerOut(O out) {
-        outQueue.offer(out);
-        if(Thread.State.TIMED_WAITING.equals(notifiedThread.getState())) {
-            notifiedThread.interrupt();
+    public final void offer(E event) {
+        if(event != null) {
+            inQueue.offer(event);
         }
     }
 
     /* 向输入队列中移除元素，如果没有输入队列元素，则睡眠指定时间 */
-    protected final I pollIn() {
+    protected final E pollInSleep() {
         if(inQueue.isEmpty()) {
             try {
                 TimeUnit.SECONDS.sleep(3);
@@ -47,5 +34,18 @@ public abstract class Executor<I, O> implements Executable<I, O> {
             }
         }
         return inQueue.poll();
+    }
+
+    /* 向输入队列中移除元素, 不睡眠 */
+    protected final E pollIn() {
+        return inQueue.poll();
+    }
+
+    /* 把事件发往下游，通知下游线程 */
+    protected final void send(E event) {
+        downStreamExecutor.offer(event);
+        if(Thread.State.TIMED_WAITING.equals(downStreamThread.getState())) {
+            downStreamThread.interrupt();
+        }
     }
 }
