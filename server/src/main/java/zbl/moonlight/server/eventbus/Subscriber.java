08897764d5365c6e3dfa8402265d51a8ec4177a5
zhangbaili <1456938262@qq.com>
2022-01-05 22:05:21 +0800

    删除事件总线模块的订阅者

--- server/src/main/java/zbl/moonlight/server/eventbus/Subscriber.java
+++
@@ -1,9 +0,0 @@
-package zbl.moonlight.server.eventbus;
-
-import zbl.moonlight.server.protocol.MdtpRequest;
-
-@FunctionalInterface
-/* 提供[事件名]和[入口队列列表] */
-public interface Subscriber {
-    void handle (MdtpRequest request);
-}
