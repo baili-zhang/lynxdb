d08fb1c395749262f6e7c14a8296aa4d4c92cd3b
zhangbaili <1456938262@qq.com>
2022-01-05 22:07:46 +0800

    删除旧的Executor测试代码

--- server/src/test/java/zbl/moonlight/server/executor/ExecutorTest.java
+++ server/src/test/java/zbl/moonlight/server/executor/ExecutorTest.java
@@ -4,45 +4,23 @@ import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ExecutorTest {
 
     /* 一个简单的执行器，将输入的数组加一 */
-    class SimpleExecutor extends Executor<Integer, Integer> {
-        public SimpleExecutor(Thread notifiedThread) {
-            super(notifiedThread);
+    class SimpleExecutor extends Executor<Integer> {
+
+        protected SimpleExecutor(Executable<Integer> downStreamExecutor, Thread downStreamThread) {
+            super(downStreamExecutor, downStreamThread);
         }
 
         @Override
         public void run() {
-            while(true) {
-                Integer in = pollIn();
-                if(in != null) {
-                    offerOut(in + 1);
-                    break;
-                }
-            }
+
         }
     }
 
     @Test
     void run() {
-        Executor<Integer, Integer> executor = new SimpleExecutor(Thread.currentThread());
-        Thread t1 = new Thread(executor);
-        t1.start();
-        executor.offer(1);
-
-        if (Thread.State.TIMED_WAITING.equals(t1.getState())) {
-            t1.interrupt();
-        }
-
-        Integer result = executor.poll();
-        while (result == null) {
-            result = executor.poll();
-            sleep();
-        }
-        assert result.equals(2);
     }
 
     void sleep() {
