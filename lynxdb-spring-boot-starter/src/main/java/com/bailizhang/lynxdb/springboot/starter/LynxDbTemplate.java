c2c2407fdb9face55629dd7e5bf9db997f2eed12
zhangbaili <1456938262@qq.com>
2022-09-07 00:14:05 +0800

    feat: Perfect LynxDbTable

--- lynxdb-spring-boot-starter/src/main/java/com/bailizhang/lynxdb/springboot/starter/LynxDbTemplate.java
+++ lynxdb-spring-boot-starter/src/main/java/com/bailizhang/lynxdb/springboot/starter/LynxDbTemplate.java
@@ -2,6 +2,7 @@ package com.bailizhang.lynxdb.springboot.starter;
 
 import com.bailizhang.lynxdb.client.AsyncLynxDbClient;
 import com.bailizhang.lynxdb.client.LynxDbFuture;
+import com.bailizhang.lynxdb.core.common.G;
 import com.bailizhang.lynxdb.core.executor.Executor;
 import com.bailizhang.lynxdb.socket.client.ServerNode;
 import com.bailizhang.lynxdb.storage.core.Column;
@@ -11,6 +12,7 @@ import com.bailizhang.lynxdb.storage.core.Pair;
 
 import java.io.IOException;
 import java.nio.channels.SelectionKey;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
@@ -42,6 +44,11 @@ public class LynxDbTemplate {
         return client.asyncCreateTableColumn(current, table, columns);
     }
 
+    public LynxDbFuture asyncCreateTableColumn(List<String> columns, String table) {
+        List<byte[]> cols = columns.stream().map(G.I::toBytes).toList();
+        return client.asyncCreateTableColumn(current, table, cols);
+    }
+
     public LynxDbFuture asyncDropKvstore(List<String> kvstores) {
         return client.asyncDropKvstore(current, kvstores);
     }
@@ -54,12 +61,28 @@ public class LynxDbTemplate {
         return client.asyncDropTableColumn(current, table, columns);
     }
 
+    public LynxDbFuture asyncDropTableColumn(String table, List<String> columns) {
+        HashSet<Column> cols = new HashSet<>();
+        columns.stream().map(G.I::toBytes).map(Column::new).forEach(cols::add);
+        return client.asyncDropTableColumn(current, table, cols);
+    }
+
     public LynxDbFuture asyncKvDelete(String kvstore, List<byte[]> keys) {
         return client.asyncKvDelete(current, kvstore, keys);
     }
 
+    public LynxDbFuture asyncKvDelete(List<String> keys, String kvstore) {
+        List<byte[]> keyList = keys.stream().map(G.I::toBytes).toList();
+        return client.asyncKvDelete(current, kvstore, keyList);
+    }
+
     public LynxDbFuture asyncKvGet(String kvstore, List<byte[]> keys) {
         return client.asyncKvGet(current, kvstore, keys);
+    }
+
+    public LynxDbFuture asyncKvGet(List<String> keys, String kvstore) {
+        List<byte[]> keyList = keys.stream().map(G.I::toBytes).toList();
+        return client.asyncKvGet(current, kvstore, keyList);
     }
 
     public LynxDbFuture asyncKvSet(String kvstore, List<Pair<byte[], byte[]>> kvPairs) {
@@ -68,6 +91,11 @@ public class LynxDbTemplate {
 
     public LynxDbFuture asyncTableDelete(String table, List<byte[]> keys) {
         return client.asyncTableDelete(current, table, keys);
+    }
+
+    public LynxDbFuture asyncTableDelete(List<String> keys, String table) {
+        List<byte[]> keyList = keys.stream().map(G.I::toBytes).toList();
+        return client.asyncTableDelete(current, table, keyList);
     }
 
     public LynxDbFuture asyncTableInsert(String table, MultiTableRows rows) {
