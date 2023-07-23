09ecbd2e0e376babdf370d8c5b100b6bf484628d
zhangbaili <1456938262@qq.com>
2023-07-23 15:45:58 +0800

    feat: Upgrade version to "2023.7.23-alpha"

--- lynxdb-script/systemctl/start-server.sh
+++ lynxdb-script/systemctl/start-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -Dlynxdb.baseDir=/root/lynxdb-v2023.7.20-snapshot/\
+java -Dlynxdb.baseDir=/root/lynxdb-v2023.7.23-alpha/\
      -Xmx256m -Xms256m\
      -XX:+UseZGC\
-     -jar /root/lynxdb-v2023.7.20-snapshot/lib/lynxdb-server-2023.7.20-snapshot.jar
+     -jar /root/lynxdb-v2023.7.23-alpha/lib/lynxdb-server-2023.7.23-alpha.jar
