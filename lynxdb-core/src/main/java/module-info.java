14afadeb72f01e188924950edd11f946002d96ff
zhangbaili <1456938262@qq.com>
2023-12-23 00:51:47 +0800

    feat: Add licence to file header

--- lynxdb-core/src/main/java/module-info.java
+++ lynxdb-core/src/main/java/module-info.java
@@ -5,7 +5,6 @@ module lynxdb.core {
     exports com.bailizhang.lynxdb.core.executor;
     exports com.bailizhang.lynxdb.core.log;
     exports com.bailizhang.lynxdb.core.mmap;
-    exports com.bailizhang.lynxdb.core.timeout;
     exports com.bailizhang.lynxdb.core.utils;
     exports com.bailizhang.lynxdb.core.recorder;
     exports com.bailizhang.lynxdb.core.arena.exceptions;
